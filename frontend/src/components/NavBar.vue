8db9f9be4c630c1680089da9bf7e612d336105fa
roger <rher92@gmail.com>
2022-01-22 01:44:17 -0300

    UPDATE: Title from Navbar and tab have been changed

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <header>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="/">Django + Vue</a>
+        <a class="navbar-brand" href="/">Inicio</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
